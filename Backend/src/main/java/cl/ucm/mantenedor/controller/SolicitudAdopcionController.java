32a76fb3e9302450513b16737e4836452124b443
F-cisterna <fabian33358@gmail.com>
2026-06-14 19:51:47 -0400

    QA al bloque 2 con ajustes

--- Backend/src/main/java/cl/ucm/mantenedor/controller/SolicitudAdopcionController.java
+++ Backend/src/main/java/cl/ucm/mantenedor/controller/SolicitudAdopcionController.java
@@ -5,7 +5,6 @@ import cl.ucm.mantenedor.dto.out.SolicitudAdopcionOutDTO;
 import cl.ucm.mantenedor.service.SolicitudAdopcionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,31 +20,26 @@ public class SolicitudAdopcionController {
     }
 
     @PostMapping
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<SolicitudAdopcionOutDTO> createSolicitud(@RequestBody SolicitudAdopcionInDTO dto) {
         return new ResponseEntity<>(service.createSolicitud(dto), HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public ResponseEntity<SolicitudAdopcionOutDTO> getSolicitudById(@PathVariable Long id) {
         return ResponseEntity.ok(service.getSolicitudById(id));
     }
 
     @GetMapping
-    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public ResponseEntity<List<SolicitudAdopcionOutDTO>> getAllSolicitudes() {
         return ResponseEntity.ok(service.getAllSolicitudes());
     }
 
     @PutMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<SolicitudAdopcionOutDTO> updateSolicitud(@PathVariable Long id, @RequestBody SolicitudAdopcionInDTO dto) {
         return ResponseEntity.ok(service.updateSolicitud(id, dto));
     }
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteSolicitud(@PathVariable Long id) {
         service.deleteSolicitud(id);
         return ResponseEntity.noContent().build();
